edb91840d64f7f988d3ce99086f86cce1b528aee
Christopher Chin <37155532+cchinchristopherj@users.noreply.github.com>
2018-03-18 17:01:33 -0700

    Remove create_model function
    Remove create_model function, as it should be defined in training.py

--- whale_cnn.py
+++ whale_cnn.py
@@ -265,36 +265,3 @@ def extract_featuresH(file,params=None):
     # Resize spectrogram image into a square matrix 
     W= resize(W,(64,64),mode='edge')
     return W
-
-def create_model():
-    ''' create_model Method
-            CNN Model for Right Whale Upcall Recognition 
-            
-            Returns: 
-                Q: Compiled Keras CNN Model
-                
-    '''
-    model = Sequential()
-    # Dropout on the visible layer (1 in 5 probability of dropout) 
-    model.add(Dropout(0.2,input_shape=(X_train.shape[1],X_train.shape[2],X_train.shape[3]),name='drop1'))
-    # Conv2D -> BatchNorm -> Relu Activation -> MaxPooling2D
-    model.add(Conv2D(15,(7,7),strides=(1,1),name='conv1'))
-    model.add(BatchNormalization(axis=3,name='bn1'))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D((2,2),name='max_pool1'))
-    # Conv2D -> BatchNorm -> Relu Activation -> MaxPooling2D
-    model.add(Conv2D(30,(7,7),strides=(1,1),name='conv2'))
-    model.add(BatchNormalization(axis=3,name='bn2'))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D((2,2),name='max_pool2'))
-    # Flatten to yield input for the fully connected layer 
-    model.add(Flatten())
-    model.add(Dense(200,activation='relu',name='fc1'))
-    # Dropout on the fully connected layer (1 in 2 probability of dropout) 
-    model.add(Dropout(0.5,name='drop2'))
-    # Single unit output layer with sigmoid nonlinearity 
-    model.add(Dense(1,activation='sigmoid',name='fc2'))
-    # Use Stochastic Gradient Discent for optimization 
-    sgd = SGD(lr=0.1,decay=0.005,nesterov=False)
-    model.compile(optimizer=sgd,loss='binary_crossentropy',metrics=['accuracy'])
-    return model
