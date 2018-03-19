16eb2e3cf8a0d754cd0204d38e3beb3ef960601f
Christopher Chin <37155532+cchinchristopherj@users.noreply.github.com>
2018-03-18 17:06:41 -0700

    Modify tuning.py
    Added "import whale_cnn" to import classes and functions from whale_cnn.py
    Modified tuning.py into a driver program for tuning the # of neurons in fully-connected layer

--- tuning.py
+++ tuning.py
@@ -1,35 +1,33 @@
 '''
 Usage:
-python whale_cnn.py
+python tuning.py
 '''
-# Convolutional Neural Network for right whale upcall recognition 
+# Tuning Number of Neurons in Fully-Connected Layer 
 
-# Spectrogram parameters 
-params = {'NFFT':256,'Fs':2000,'noverlap':192}
-# Load in the audio files from the training dataset
-path = 'Documents/Bioacoustics_MachineLearning/train2'
-filenames = glob.glob(path+'/*.aif')
-# Use a subset of the ~47000 files in the training dataset for hyperparameter 
-# optimization
-num_files = 5000
-# For each audio file, extract the spectrograms with vertically-enhanced contrast 
-# separately from the spectrograms with horizontally-enhanced contrast. This in 
-# effect doubles the amount of data for tuning, and presents the CNN with different 
-# perspectives of the same spectrogram image of the original audio file 
-training_featuresV = np.array([extract_featuresV(x,params=params) for x in filenames[0:num_files]])
-training_featuresH = np.array([extract_featuresH(x,params=params) for x in filenames[0:num_files]])
-# Concatenate the two feature matrices together to form a double-length feature matrix
-X_train = np.append(training_featuresV,training_featuresH,axis=0)
-# Axis 0 indicates the number of examples, Axis 1 and 2 are the features (64x64 image
-# spectrograms). Add Axis 3 to indicate 1 channel (depth of 1 for spectrogram image) for 
-# compatibility with Keras CNN model 
-X_train = X_train[:,:,:,np.newaxis]
-# Extract labels for the training dataset. Since the vertically-enhanced and 
-# horizontally-enhanced images are concatenated to form a training dataset twice as long,
-# append a duplicate copy of the training labels to form a training label vector 
-# twice as long 
-Y_train = np.array([extract_labels(x) for x in filenames[0:num_files]])
-Y_train = np.append(Y_train,Y_train)
+import whale_cnn
+import numpy as np
+import matplotlib.pyplot as plt
+import glob
+from skimage.transform import resize
+import aifc
+import scipy.signal as sp
+import scipy
+import pylab as pl
+import os
+from sklearn.metrics import roc_auc_score
+from matplotlib import mlab
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.callbacks import Callback
+from keras import layers
+from keras.layers import Dense, Dropout, Activation, BatchNormalization, Flatten, Conv2D, MaxPooling2D
+from keras.models import load_model, Sequential
+from keras.optimizers import SGD
+from keras.utils import plot_model
+from matplotlib.pyplot import imshow
+import keras.backend as K
+K.set_image_data_format('channels_last')
+get_ipython().magic('matplotlib inline')
+plt.rcParams['figure.figsize'] = (13,5)
 
 def create_model(neurons=100):
     ''' create_model Method
@@ -63,6 +61,33 @@ def create_model(neurons=100):
     sgd = SGD(lr=0.1,decay=0.005,nesterov=False)
     model.compile(optimizer=sgd,loss='binary_crossentropy',metrics=['accuracy'])
     return model
+
+# Spectrogram parameters 
+params = {'NFFT':256,'Fs':2000,'noverlap':192}
+# Load in the audio files from the training dataset
+path = 'Documents/Bioacoustics_MachineLearning/train2'
+filenames = glob.glob(path+'/*.aif')
+# Use a subset of the ~47000 files in the training dataset for hyperparameter 
+# optimization
+num_files = 5000
+# For each audio file, extract the spectrograms with vertically-enhanced contrast 
+# separately from the spectrograms with horizontally-enhanced contrast. This in 
+# effect doubles the amount of data for tuning, and presents the CNN with different 
+# perspectives of the same spectrogram image of the original audio file 
+training_featuresV = np.array([extract_featuresV(x,params=params) for x in filenames[0:num_files]])
+training_featuresH = np.array([extract_featuresH(x,params=params) for x in filenames[0:num_files]])
+# Concatenate the two feature matrices together to form a double-length feature matrix
+X_train = np.append(training_featuresV,training_featuresH,axis=0)
+# Axis 0 indicates the number of examples, Axis 1 and 2 are the features (64x64 image
+# spectrograms). Add Axis 3 to indicate 1 channel (depth of 1 for spectrogram image) for 
+# compatibility with Keras CNN model 
+X_train = X_train[:,:,:,np.newaxis]
+# Extract labels for the training dataset. Since the vertically-enhanced and 
+# horizontally-enhanced images are concatenated to form a training dataset twice as long,
+# append a duplicate copy of the training labels to form a training label vector 
+# twice as long 
+Y_train = np.array([extract_labels(x) for x in filenames[0:num_files]])
+Y_train = np.append(Y_train,Y_train)
 
 # Use sklearn wrapper on the Keras model to be able to use sklearn methods 
 # such as GridSearchCV
